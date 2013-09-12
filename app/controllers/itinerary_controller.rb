c392b8a7d0236dad3ec194fad1e1204bf2d2dae6
DaleSwanson <crazysteve138@hotmail.com>
2013-09-12 05:12:48 -0400

    added alias table, display basic stats on display page

--- app/controllers/itinerary_controller.rb
+++ app/controllers/itinerary_controller.rb
@@ -1,13 +1,26 @@
 class ItineraryController < ApplicationController
-	def create
-		@points = Point.all;
-	end
-	def display
-		@route   = params[:route]
-		@hScale  = params[:h]
-		@dScale  = params[:d]
-		@points = @route.split(';');
-		
-	end
-	
+  def create
+    @points = Point.all;
+  end
+
+  def display
+    @route   = params[:route]
+    @hScale  = params[:h]
+    @dScale  = params[:d]
+    @points = @route.split(';');
+    @pairs = Pair.all;
+    @aliases = Alias.all;
+    @segments = [];
+    @listOfTimes = [];
+    @listOfDistances = [];
+    temp=''
+    @points.each_with_index do |point, i|
+      if @points.to_a[i+1].nil? then break end
+      next_point = @points.to_a[i+1]
+      this_id = Alias.where(name: point).first.point_id;
+      next_id = Alias.where(name: next_point).first.point_id;
+      @segments << Pair.where(from_id: this_id, to_id: next_id).first;
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130912025814) do
+ActiveRecord::Schema.define(:version => 20130912075400) do
+
+  create_table "aliases", :force => true do |t|
+    t.string   "name"
+    t.integer  "point_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pairs", :force => true do |t|
     t.integer  "from_id"
