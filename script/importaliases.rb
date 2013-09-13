3f944826c49d6ae2dac791f4638ebb7b8d7ba60a
DaleSwanson <crazysteve138@hotmail.com>
2013-09-13 05:26:53 -0400

    import city travel times

--- script/importaliases.rb
+++ script/importaliases.rb
@@ -1,6 +1,6 @@
 require 'csv'
 ActiveRecord::Base.transaction do
-  CSV.open('script/alias.csv', 'r').each do |row|
+  CSV.open('script/citiesabv.csv', 'r').each do |row|
     Alias.create(name: row[0], point_id: row[1])
   end
 end
